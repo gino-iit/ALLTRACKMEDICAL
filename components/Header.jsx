193d164c25777d538d6d347e927a3637193a6598
gino <gi-no@live.nl>
2022-01-11 09:38:51 +0100

    PWA

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,6 +7,7 @@ import fetchJson from 'lib/fetchJson'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, LogoutIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import Head from 'next/head'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -27,10 +28,15 @@ export function HeaderNav() {
     <Disclosure as="nav" className="bg-red-600">
       {({ open }) => (
         <>
+             <Head>
+             <link rel="manifest" href="./manifest.json" />
+
+</Head>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
+             
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
